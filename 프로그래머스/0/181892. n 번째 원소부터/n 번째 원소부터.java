d9d0a7722a9e9ceddf28d3077cfd21cf1f13b373
YOUNGMINKIM <53969142+ym1085@users.noreply.github.com>
2026-03-25 23:18:53 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.04 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,11 +1,16 @@
 import java.util.*;
-
+    
 class Solution {
     public List<Integer> solution(int[] num_list, int n) {
-        List<Integer> answer = new ArrayList<>();
-        for (int i = n; i <= num_list.length; i++) {
-            answer.add(num_list[i - 1]);
+        List<Integer> result = new ArrayList<>();
+        
+        // n(3)번째 원소부터 마지막 원소까지의 모든 원소를 담은 배열을 반환
+        for (int i = 0; i < num_list.length; i++) {
+            // 1, 2, 3
+            if ((i + 1) >= n) {
+                result.add(num_list[i]);
+            }
         }
-        return answer;
+        return result;
     }
 }
