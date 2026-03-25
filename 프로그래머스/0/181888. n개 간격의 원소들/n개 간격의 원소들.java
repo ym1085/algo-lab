ce81c4a679f6daa807caf64ab8663788432180fc
YOUNGMINKIM <53969142+ym1085@users.noreply.github.com>
2026-03-25 23:02:17 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(int[] num_list, int n) {
-        List<Integer> answer = new ArrayList<>();
-        
-        // [4, 2, 6, 1, 7, 6]
-        // 4, 6, 7
-        for (int i = 0; i < num_list.length; i+=n) {
-            answer.add(num_list[i]);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % n == 0) {
+                result.add(num_list[i]);
+            }
         }
-        return answer;
+        return result;
     }
 }
