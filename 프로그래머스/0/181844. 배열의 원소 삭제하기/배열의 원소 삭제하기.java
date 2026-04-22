3cff543f7dc8fe0d7c446ec3374841d86be906fb
YOUNGMINKIM <53969142+ym1085@users.noreply.github.com>
2026-04-22 20:56:20 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 4.21 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,20 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public List solution(int[] arr, int[] delete_list) {
-        List<Integer> answer = new ArrayList<>();
-        
-        for (int num : arr) {
-            answer.add(num);
+    
+    // 1번 방법 : List의 remove 사용
+    // public List solution(int[] arr, int[] delete_list) {
+    //     List<Integer> answer = new ArrayList<>();
+    //     for (int num : arr) {
+    //         answer.add(num);
+    //     }
+    //     for (int i = 0; i < arr.length; i++) {
+    //         for (int j = 0; j < delete_list.length; j++){
+    //             if (arr[i] == delete_list[j]) {
+    //                 answer.remove(Integer.valueOf(arr[i]));
+    //             }
+    //         }
+    //     }
+    //     return answer;
+    // }
+    
+    // 2번 방법 : Set 사용
+    public int[] solution(int[] arr, int[] delete_list) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : delete_list) {
+            set.add(num);
         }
-        
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < delete_list.length; j++) {
-                if (arr[i] == delete_list[j]) {
-                    answer.remove(Integer.valueOf(arr[i]));
-                }
-            }
-        }
-        return answer;
+        return Arrays.stream(arr).filter(i -> !set.contains(i)).toArray();
     }
 }
